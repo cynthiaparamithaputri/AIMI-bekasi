045c92b71032b60e3ef5c34cf9200fab1e4302ee
cynthiaparamithaputri <cynthiaparamithaputri@gmail.com>
2023-07-15 10:31:44 +0700

    15 juli

--- src/pages/TambahKegiatan.jsx
+++ src/pages/TambahKegiatan.jsx
@@ -1,6 +1,47 @@
+import {Container, Row, Col, Form, Button} from "react-bootstrap"
+import NavbarAdmin from "../components/NavbarAdmin"
+
 const TambahKegiatan = () => {
   return (
-    <div>TambahKegiatan</div>
+    <div className="container-fluid tambah min-vh-100">
+      <NavbarAdmin />
+        <div className="tambah-kegkon w-100 min-vh-100">
+        <Container>
+          <Row className="tambah-row">
+          <h1 className="fw-bold text-center mb-5">Tambah Kegiatan</h1>
+          <Form>
+
+          <Row className="mb-3">
+          <Form.Group as={Col} controlId="formGridJudul">
+            <Form.Label>Nama Kegiatan</Form.Label>
+            <Form.Control />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="formGridTGL">
+            <Form.Label>Jadwal</Form.Label>
+            <Form.Control placeholder="Contoh: Sabtu, 15 Juli 2023" />
+          </Form.Group>
+          </Row>
+
+          <Form.Group className="mb-3" controlId="FormGridDesk">
+            <Form.Label>Deskripsi</Form.Label>
+            <Form.Control as="textarea" rows={3} />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formFile">
+            <Form.Label>Upload Foto</Form.Label>
+            <Form.Control type="file" />
+          </Form.Group>
+
+          <Button className="mt-3" type="submit">
+            Submit
+          </Button>
+        
+        </Form>
+          </Row>
+        </Container>
+      </div>
+    </div>
   )
 }
 
--- src/pages/TambahKonselor.jsx
+++ src/pages/TambahKonselor.jsx
@@ -1,6 +1,40 @@
+import {Container, Row, Col, Form, Button} from "react-bootstrap"
+import NavbarAdmin from "../components/NavbarAdmin"
+
 const TambahKonselor = () => {
   return (
-    <div>TambahKonselor</div>
+    <div className="container-fluid tambah min-vh-100">
+      <NavbarAdmin />
+        <div className="tambah-kegkon w-100 min-vh-100">
+        <Container>
+          <Row className="tambah-row">
+          <h1 className="fw-bold text-center mb-5">Tambah Konselor</h1>
+          <Form>
+
+          <Form.Group className="mb-3" controlId="formGridNamaKon">
+            <Form.Label>Nama Konselor</Form.Label>
+            <Form.Control />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formGridWAKon">
+            <Form.Label>No WA</Form.Label>
+            <Form.Control />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formGridEmailKon">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" />
+          </Form.Group>
+
+          <Button className="mt-3" type="submit">
+            Submit
+          </Button>
+        
+        </Form>
+          </Row>
+        </Container>
+      </div>
+    </div>
   )
 }
 
